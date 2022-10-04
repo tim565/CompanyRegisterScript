212930df5e71795011a2471e60dbb7fd67c38530
tim <schmitz.tim@protonmail.com>
2022-10-04 08:35:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 18:15:59 2022 at Grenoble École de Management (France)
+
+@author: tim565
+"""
+
+print("--- Company Register Started ---")
+q = input("q: ")
+url = "https://www.wlw.de/de/suche?q=" + q
+
+locationInput = input("Insert y to add a location, insert n for no location, then press enter: ")
+if locationInput == "y":
+    locationCountry = "DE"
+    url = url + "&locationCountry=" + locationCountry
+    locationKind = "other"
+    url = url + "&locationKind=" + locationKind
+
+    locationLatitude = input("locationLatitude: ")
+    url = url + "&locationLatitude=" + locationLatitude
+    locationLongitude = input("locationLongitude: ")
+    url = url + "&locationLongitude=" + locationLongitude
+
+    locationName = input("locationName: ")
+    url = url + "&locationName=" + locationName + ",%20Deutschland"
+
+    locationRadius = input("locationRadiusInput: ")
+    url = url + "&locationRadius=" + locationRadius + "km"
+
+supplierTypesInput = input("Insert y to add a supplier type, insert n for no supplier type, then press enter: ")
+if supplierTypesInput == "y":
+    supplierTypes = input("Use one or more of these: Hersteller_Händler_Dienstleister_Großhändler; supplierTypes: ")
+    url = url + "&supplierTypes=" + supplierTypes
+
+employeeCountsInput = input("Insert y to add number of employees, insert n for no number of employees then press enter:")
+if employeeCountsInput == "y":
+    employeeCounts = input("Use this format: 10-49_50-199_200%2B; emplyeeCounts: ")
+    url = url + "&employeeCounts=" + employeeCounts
+
+print("Full url:", url)
+
+# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+input("Press enter to start company search")
+# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+company_name_list = []
+company_plz_city_list = []
+company_description_list = []
+
+#from selenium import webdriver
+
+#System.setProperty("webdriver.gecko.driver", "./geckodriver.exe")
+#DesiredCapabilities capabilities = DesiredCapabilities.firefox()
+#capabilities.setCapability("marionette",true)
+#driver= new FirefoxDriver(capabilities)
+
+#driver.get(url)
+
+
+
+
+
+print("--- Company Register Finished --- (Code: 0) ")
+
+
+
+

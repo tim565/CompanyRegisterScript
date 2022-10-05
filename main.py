6db148374ec606c5b7e7f0484518987da7250332
tim <schmitz.tim@protonmail.com>
2022-10-05 12:19:50 +0200

    Get and print list of 30 items from website

--- main.py
+++ main.py
@@ -64,16 +64,30 @@ btn_cookie = driver.find_element(By.ID, "CybotCookiebotDialogFooterButtonAcceptA
 btn_cookie.click()
 print("Info: Cookie button clicked \nInfo: Ready to start search")
 
-print("Get company name of first: ")
+# TODO: specify page number before iterating (by change in the URL)
 
-test = driver.find_element(By.XPATH, "//div[@class='flex flex-col']/div[1]/a[@class='company-title-link']").get_attribute("innerHTML")
-print(test)
-
-
-# For later use
+# Iterate through list with 30 (default) items
 company_name_list = []
 company_plz_city_list = []
 company_description_list = []
+
+for i in range(30):
+    # TODO: currently only full sites with 30 items can be accessed; check for next item and break if necessary
+    path_company_name = "//div[@class='flex flex-col']/div[" + str(i+1) + "]/a[@class='company-title-link']"
+    company_name = driver.find_element(By.XPATH, path_company_name).get_attribute("innerHTML")
+    company_name_list.append(company_name)
+
+    path_company_plz_city = "//div[@class='flex flex-col']/div[" + str(i+1) + "]//div[@class='address']"
+    company_plz_city = driver.find_element(By.XPATH, path_company_plz_city).get_attribute("innerHTML")
+    company_plz_city_list.append(company_plz_city)
+
+    path_company_description = "//div[@class='flex flex-col']/div[" + str(i+1) + "]/div[@class='description']"
+    company_description = driver.find_element(By.XPATH, path_company_description).get_attribute("innerHTML")
+    company_description_list.append(company_description)
+
+print(company_name_list)
+print(company_plz_city_list)
+print(company_description_list)
 
 """
 Clean and close

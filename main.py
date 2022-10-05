bb9db0a5c5bd61befebb83cb42d4733ae10c7262
tim <schmitz.tim@protonmail.com>
2022-10-05 10:36:38 +0200

    Remove cookie button when starting website

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct  3 18:15:59 2022 at Grenoble École de Management (France)
+Created on Mon Oct  3 18:15:59 2022 at Grenoble École de Management (Grenoble, France)
 
 @author: tim565
 """
@@ -9,6 +9,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.common.by import By
+
+"""
+The user needs to specify parameters for the search in the database. 
+The parameter 'q' is the only mandatory, it specifies a word for the search engine. All other params are optional. 
+"""
 print("--- Company Register Started ---")
 q = input("q: ")
 url = "https://www.wlw.de/de/suche?q=" + q
@@ -42,28 +48,33 @@ if employeeCountsInput == "y":
     url = url + "&employeeCounts=" + employeeCounts
 
 print("Full url:", url)
-
-# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 input("Press enter to start company search")
-# ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
+"""
+After the search params are entered and the URL is generated, the script uses selenium to access the website. 
+"""
+# Start webdriver and wait 5 seconds until the website has load and the cookie button popped up
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get(url)
+print("Info: Loading website...")
+time.sleep(5)
+
+# Remove cookie button
+btn_cookie = driver.find_element(By.ID, "CybotCookiebotDialogFooterButtonAcceptAll")
+btn_cookie.click()
+print("Info: Cookie button clicked \nInfo: Ready to start search")
+
+"""
+TODO: next steps
+"""
 company_name_list = []
 company_plz_city_list = []
 company_description_list = []
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get(url)
-
-time.sleep(100)
-
+"""
+Clean and close
+"""
+time.sleep(10)
+print("Info: Timer out, quitting website...")
 driver.quit()
-
-
-
-
-
-
-
-
-
-# print("--- Company Register Finished --- (Code: 0) ")
+print("--- Company Register Finished --- (Code: 0) ")
